273008a1b089096b860d7a01eca26118286664e0
EnesAgcahan <98733474+EnesAgcahan@users.noreply.github.com>
2022-08-19 19:13:39 +0300

    EnesAgcahan_Hafta_2

---
+++ EnesAgcahan_Hafta_2/Staff.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace EnesAgcahan_Hafta_2
+{
+    public class Staff
+    {
+        public int Id { get; set; }
+
+        [MinLength(4), MaxLength(200), RegularExpression(@"^[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]+$", ErrorMessage = "Invalid username")]
+        public string Name { get; set; }
+
+        [MinLength(4), MaxLength(200), RegularExpression(@"^[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]+$", ErrorMessage = "Invalid username")]
+        public string Lastname { get; set; }
+
+        [Range(typeof(DateTime), " 11/11/1945", "10/10/2002",
+            ErrorMessage = "Valid dates for the Property {0} between {1} and {2}")]
+        public DateTime DateOfBirth { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z0-9!@#\$%\^\&*\)\(+=_-]+$", ErrorMessage = "Email is not valid.")]
+        public string Email { get; set; }
+
+        [Phone(ErrorMessage = "Phone is not valid.")]
+        [RegularExpression(@"^(\+[0-9]{9})$")]
+        public int PhoneNumber { get; set; }
+
+        [MinLength(2000), MaxLength(9000)]
+        public int Salary { get; set; }
+
+
+
+
+
+        public Staff()
+        {
+
+        }
+    }
+}
---
+++ test1/Data/test1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EnesAgcahan_Hafta_2;
+
+namespace test1.Data
+{
+    public class test1Context : DbContext
+    {
+        public test1Context (DbContextOptions<test1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EnesAgcahan_Hafta_2.Staff> Staff { get; set; } = default!;
+    }
+}
